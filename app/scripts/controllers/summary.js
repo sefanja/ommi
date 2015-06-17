6853c00785b7625f6a87595eef4a8407e2262d37
jacobusklok <jacobus.klok@yahoo.nl>
2015-06-17 16:24:22 +0200

    Tab summary
    Tab summary ook via json beschikbaar

--- app/scripts/controllers/summary.js
+++ app/scripts/controllers/summary.js
@@ -8,10 +8,19 @@
  * Controller of the ommiApp
  */
 angular.module('ommiApp')
-  .controller('SummaryCtrl', function ($scope) {
+  .controller('SummaryCtrl', function ($scope, $http, $modal, $sce) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('api/summary.json').success(function (data) {
+      $scope.summary = data;
+    });
+
+    $scope.renderHtml = function (htmlCode) {
+      return $sce.trustAsHtml(htmlCode);
+    };
+
   });
