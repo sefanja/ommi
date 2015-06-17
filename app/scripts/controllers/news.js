6806760d906f6a2a93d815d3761f70ba59a907bb
jacobusklok <jacobus.klok@yahoo.nl>
2015-06-17 14:23:28 +0200

    News tab
    News tab toegevoegd

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -2,16 +2,39 @@
 
 /**
  * @ngdoc function
- * @name ommiApp.controller:NewsCtrl
+ * @name ommiApp.controller:CvCtrl
  * @description
- * # NewsCtrl
+ * # CvCtrl
  * Controller of the ommiApp
  */
 angular.module('ommiApp')
-  .controller('NewsCtrl', function ($scope) {
+  .controller('NewsCtrl', function ($scope, $http, $modal, $sce) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('api/news.json').success(function (data) {
+      $scope.newsitems = data;
+    });
+
+    $scope.renderHtml = function (htmlCode) {
+      return $sce.trustAsHtml(htmlCode);
+    };
+
+    $scope.predicate = 'date';
+    $scope.reverse = true;
+    $scope.order = function(predicate) {
+      $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+      $scope.predicate = predicate;
+    };
+
+    $scope.formatDate =  function(date) {
+      var formatedDate = new Date(date);
+      var day = formatedDate.getDate()
+      var month = formatedDate.getMonth()
+
+      return  (day<10?"0"+day:day) +"-" + (month<10?"0"+month:month) +"-"+ formatedDate.getFullYear();
+    }
   });
