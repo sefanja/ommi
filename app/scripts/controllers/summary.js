38a99bbe2d2d177a06922d2c9a98c5fc0ea64c9e
unknown <50402sse@CLD89D67CB044A.staff.eur.nl>
2015-06-20 17:39:41 +0200

    some minor touches, made summary content editable

--- app/scripts/controllers/summary.js
+++ app/scripts/controllers/summary.js
@@ -8,19 +8,14 @@
  * Controller of the ommiApp
  */
 angular.module('ommiApp')
-  .controller('SummaryCtrl', function ($scope, $http, $modal, $sce) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    $http.get('api/summary.json').success(function (data) {
-      $scope.summary = data;
-    });
+  .controller('SummaryCtrl', function ($rootScope, $scope, $http, $modal, $sce) {
+    if (!$rootScope.summary) {
+      $http.get('api/summary.json').success(function (data) {
+        $rootScope.summary = data;  // Bind to root scope for long-term memory of editable content.
+      });
+    }
 
     $scope.renderHtml = function (htmlCode) {
       return $sce.trustAsHtml(htmlCode);
     };
-
   });
